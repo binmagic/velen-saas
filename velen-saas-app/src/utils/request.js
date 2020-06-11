4b76256a74e31a07ac8024e43f6422b9676ad0a3
罗海云 <luohaiyun@5agam.cn>
2020-06-11 12:21:38 +0800

    编译

--- velen-saas-app/src/utils/request.js
+++ velen-saas-app/src/utils/request.js
@@ -3,11 +3,12 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
+
 // create an axios instance
 const service = axios.create({
   // baseURL: "http://192.168.1.26:8091/",//process.env.VUE_APP_BASE_API, // url = base url + request url
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: true, // send cookies when cross-domain requests
   timeout: -1 // request timeout
 })
 
@@ -16,12 +17,12 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
-    }
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
+    //   config.headers['X-Token'] = getToken()
+    // }
     return config
   },
   error => {
