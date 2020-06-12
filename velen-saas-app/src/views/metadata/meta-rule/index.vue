4c77cf4426b61bde7cec2e052c2e4ff7383525ce
liuzhihao <liuzhihao@5agame.cn>
2020-06-12 19:52:07 +0800

    入库规则

--- velen-saas-app/src/views/metadata/meta-rule/index.vue
+++ velen-saas-app/src/views/metadata/meta-rule/index.vue
@@ -3,7 +3,7 @@
     <div class="meta-rule-title">
       <p>入库校验规则设置</p>
     </div>
-    <el-card>
+    <el-card style="margin-bottom: 20px">
       <p class="meta-card-header">
         事件与事件属性的入库设置
       </p>
@@ -14,16 +14,104 @@
         <el-checkbox>强校验模式</el-checkbox>
         <el-form label-position="top">
           <el-form-item label="解析格式:">
-            <el-select v-model="select" size="mini">
+            <el-select v-model="select" size="mini" style="margin-bottom: 10px;">
               <el-option v-for="role in roles" :key="role.value" :label="role.label" :value="role.value"></el-option>
             </el-select>
 <!--            <i class="el-icon-plus"></i>-->
             <el-row>
               <el-col :span="3">
-                <el-input type="textarea" v-model="select" :disabled="select==='JSON解析'?true:false" :autosize="{ minRows:2, maxRows:4 }"></el-input>
+                <el-input type="textarea" v-model="select" :disabled="select==='JSON解析'?true:false" :autosize="{ minRows:2, maxRows:4 }"/>
               </el-col>
             </el-row>
           </el-form-item>
+          <el-row>
+            <el-col :span="4" style="margin-right: 50px;">
+              <span>接收用户「登录ID」</span>
+              <p class="meta-key-desc">用户在产品内的唯一标识,不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="4" style="margin-right: 50px;">
+              <span>接收用户「设备ID」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="4" style="margin-right: 50px;">
+              <span>接收用户「事件时间」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="4" style="margin-right: 50px;">
+              <span>接收用户「事件名」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="4">
+              <span>接收用户「项目名」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+    </el-card>
+    <el-card>
+      <p class="meta-card-header">
+        用户属性入库设置
+      </p>
+      <p style="margin: 0;font-size: 14px;">
+        上报用户属性的类型与「用户属性」已有事件的类型不一致时，系统将按规则自动转换，如无法自动转换则视为异常数据不能入库。
+      </p>
+      <div class="meta-card-form">
+        <el-checkbox>强校验模式</el-checkbox>
+        <el-form label-position="top">
+          <el-form-item label="主题设置:">
+            <el-row>
+              <el-col :span="4">
+                <el-input size="small" v-model="themeSet" placeholder="多个主题以,隔开"></el-input>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item label="解析格式:">
+            <el-select v-model="select" size="mini" style="margin-bottom: 10px;">
+              <el-option v-for="role in roles" :key="role.value" :label="role.label" :value="role.value"></el-option>
+            </el-select>
+            <el-row>
+              <el-col :span="3">
+                <el-input type="textarea" v-model="select" :disabled="select==='JSON解析'?true:false" :autosize="{ minRows:2, maxRows:4 }"/>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-row>
+            <el-col :span="5">
+              <span>接收用户「登录ID」</span>
+              <p class="meta-key-desc">用户在产品内的唯一标识,不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="5">
+              <span>接收用户「设备ID」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="5">
+              <span>接收用户「时间」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+            <el-col :span="5">
+              <span>接收用户「项目名」</span>
+              <p class="meta-key-desc">不符合以下规则时,不能入库</p>
+              <p>Key规则 <el-link type="primary">默认规则</el-link></p>
+              <p>入库规则 <el-link type="primary">未设置任意格式ID均可上报入库</el-link></p>
+            </el-col>
+          </el-row>
         </el-form>
       </div>
     </el-card>
@@ -44,7 +132,8 @@
         }, {
           label: '分隔符解析',
           value: '分隔符解析'
-        }]
+        }],
+        themeSet:'',
       }
     }
   }
---
+++ velen-saas-app/src/views/metadata/meta-rule/key-dialog/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-dialog :visible.sync="visible" :before-close="handleClose">
+    <template slot="title">
+      <p>Key规则设置</p>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    props:{
+      visible:{
+        required:true,
+        type:Boolean,
+      }
+    },
+    methods: {
+      handleClose() {
+        this.$emit('close-key-rule')
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
